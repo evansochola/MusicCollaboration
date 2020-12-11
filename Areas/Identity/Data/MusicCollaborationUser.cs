fd5132fc9e3b25919d650266cde2646d847788ee
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2020-12-11 13:26:16 +0300

    Add project files.

---
+++ Areas/Identity/Data/MusicCollaborationUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MusicCollaboration.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MusicCollaborationUser class
+    public class MusicCollaborationUser : IdentityUser
+    {
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MusicCollaboration.Areas.Identity.Data;
+using MusicCollaboration.Data;
+
+[assembly: HostingStartup(typeof(MusicCollaboration.Areas.Identity.IdentityHostingStartup))]
+namespace MusicCollaboration.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MusicCollaborationContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MusicCollaborationContextConnection")));
+
+                services.AddDefaultIdentity<MusicCollaborationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MusicCollaborationContext>();
+            });
+        }
+    }
+}
