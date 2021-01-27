4411947aa531190cb22ec43327274ecb60c85667
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2021-01-27 16:54:10 +0300

    Profile Controller Added

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MusicCollaboration.Controllers
 {
+    [Authorize]
     public class ProfileController : Controller
     {
         // GET: ProfileController
