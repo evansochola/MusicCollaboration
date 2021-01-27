13f0677686965b723ef312df6230dba7adaced5c
evans.ochieng@strathmore.edu <evans.ochieng@strathmore.edu>
2021-01-27 16:54:44 +0300

    Added a new Model, CollaborationMember

---
+++ Models/CollaborationMember.cs
@@ -0,0 +1,26 @@
+﻿using MusicCollaboration.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCollaboration.Models
+{
+    public class CollaborationMember
+    {
+        public MusicCollaborationUser UserID { get; set; }
+        public Collaboration Collaboration { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        ProjectOwner,
+        Vocalist,
+        Instrumentalist,
+        Producer,
+        MixingEngineer,
+        MasteringEngineer
+    }
+}
